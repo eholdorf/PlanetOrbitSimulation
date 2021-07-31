c56c276655d000219d8f1b484d6a4a8ac13d5f57
eholdorf <87960240+eholdorf@users.noreply.github.com>
2021-07-31 15:50:51 +1000

    Add files via upload

--- simulating orbits.py
+++ simulating orbits.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.table import Table
 import model_parameters as models
+from mpl_toolkits import mplot3d
 
 # testing Mike's code for planet simulation
 '''
@@ -23,7 +24,9 @@ plt.clf()
 plt.axis('equal')
 plt.plot(rho*np.cos(np.radians(theta)), rho*np.sin(np.radians(theta)))
 
-dd = Table.read('HGCA_vEDR3.fits')
+# importing the data
+data = Table.read('HGCA_vEDR3.fits')
+data_in_30pc = [data[i] for i in np.where((data['parallax_gaia']>33))[0]]
 
 a = models.interpolate_semimajoraxis()
 T = models.interpolate_period(a)
@@ -34,7 +37,39 @@ e = models.interpolate_eccentricity()
 
 def simulate_orbit(time= 0, T=0, a=0, e=0, Omega=0, omega=0, i=0, dates=0, 
                    random = False):
-    
+    '''
+
+    Parameters
+    ----------
+    time : TYPE, optional
+        DESCRIPTION. The default is 0.
+    T : TYPE, optional
+        DESCRIPTION. The default is 0.
+    a : TYPE, optional
+        DESCRIPTION. The default is 0.
+    e : TYPE, optional
+        DESCRIPTION. The default is 0.
+    Omega : TYPE, optional
+        DESCRIPTION. The default is 0.
+    omega : TYPE, optional
+        DESCRIPTION. The default is 0.
+    i : TYPE, optional
+        DESCRIPTION. The default is 0.
+    dates : TYPE, optional
+        DESCRIPTION. The default is 0.
+    random : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Raises
+    ------
+    ValueError
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
     if random == False and a == 0:
         raise ValueError('No values given and not a random orbit.')
      
@@ -64,5 +99,12 @@ def simulate_orbit(time= 0, T=0, a=0, e=0, Omega=0, omega=0, i=0, dates=0,
     plt.figure()
     plt.clf()
     plt.axis('equal')
-    plt.plot(rho*np.cos(np.radians(theta)), rho*np.sin(np.radians(theta)))
+    plt.plot(rho*np.cos(np.radians(theta)), rho*np.sin(np.radians(theta)),'k')
+    
+    plt.figure()
+    plt.clf()
+    ax = plt.axes(projection='3d')
+    ax.plot(rho*np.cos(np.radians(theta)), rho*np.sin(np.radians(theta)),'k')
+    ax.plot([0],[0],[0],'o', color = 'orange')
+    
     
